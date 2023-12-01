e96bf8dc2857d224a0c99f74c57aafb9514e3d52
SumonHossainJs <sumonhosainrakib@gmail.com>
2023-12-01 15:11:37 +0300

    feat(navbar and footer ( nav.jsx footer.jsx): al hamdulilah

--- portfolio_next.js/src/components/footer/Footer.jsx
+++ portfolio_next.js/src/components/footer/Footer.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import styles from './footer.module.css'
+import Image from 'next/image'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={styles.container}>
+      <div>@2023 sumon, All right reserved</div>
+      <div className={styles.social}>
+          <Image src={"/1.png"} width={15} height={15} className={styles.icon} alt='faceboo'/>
+          <Image src={"/2.png"} width={15} height={15} className={styles.icon} alt='faceboo'/>
+          <Image src={"/3.png"} width={15} height={15} className={styles.icon} alt='faceboo'/>
+          <Image src={"/4.png"} width={15} height={15} className={styles.icon} alt='faceboo'/>
+      </div>
+    </div>
   )
 }
 
