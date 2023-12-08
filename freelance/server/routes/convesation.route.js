bb4749871db42921eec91efda697613c1590a788
SumonHossainJs <sumonhosainrakib@gmail.com>
2023-12-08 21:50:16 +0300

    Server is completed

--- freelance/server/routes/convesation.route.js
+++ freelance/server/routes/convesation.route.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import { verifyToken } from '../middleware/jwt.js';
+import { createConversation, getConversations, getSingleConversation, updateConversation } from '../controllers/conversation.control.js';
 
 
 
 const router = express.Router();
 
-router.post('/',verifyToken, );
-router.get('/:id', verifyToken, );
+router.post('/',verifyToken, createConversation);
+router.get('/:id', verifyToken,getConversations );
+router.get('/single/:id', verifyToken, getSingleConversation);
+router.put('/:id', verifyToken, updateConversation);
 
 
 
