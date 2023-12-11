5b66b3cb7ee5820213920ee2b262dc12f7c2e4bf
SumonHossainJs <sumonhosainrakib@gmail.com>
2023-12-11 20:46:24 +0300

    Al hamdulilah Subhan allah

--- portfolio_next.js/src/app/dashboard/page.jsx
+++ portfolio_next.js/src/app/dashboard/page.jsx
@@ -2,15 +2,24 @@
 import React, { useReducer } from 'react'
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import useSWR from 'swr';
+import Image from 'next/image';
 
 
 
 
 
 const DashBoard = () => {
-  console.log(useRouter)
   const router = useRouter()
   console.log(router)
+
+
+  const fetcher = (...args) => fetch(...args).then((res)=> res.json());
+
+
+  const { data, mutate, error, isLoading } = useSWR(
+    `/api/posts/username=${session?.data?.user.name}`, fetcher
+  )
 
 const session = useSession();
 
