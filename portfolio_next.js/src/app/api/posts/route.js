dabe6ce95300e488d1f9420bd438df296875a558
SumonHossainJs <sumonhosainrakib@gmail.com>
2023-12-11 21:29:59 +0300

    subhan allah

--- portfolio_next.js/src/app/api/posts/route.js
+++ portfolio_next.js/src/app/api/posts/route.js
@@ -3,14 +3,32 @@ import Connect from "@/utils/db";
 import Post from "@/models/Post";
 
 export const GET = async (request) => {
+    const url = new URL(request.url);
+    const username = url.searchParams.get('username');
     try{
         await Connect();
-        console.log("connected to the Database");
-        return new NextResponse("Al hamdulilah", {status:200});
+
+        const posts = await Post.find(username && {username});
+        return new NextResponse(JSON.stringify(posts),{status:200});
+        console.log(posts);
+
     }catch(err){
 
         return new NextResponse('Database ERRor', {status: 500});
     }
     
+}
+
+
+export const POST = async (request) =>{
+    const body = await  request.json();
+    const newPost = new Post(body);
+    try{
+        await Connect();
+        const savedPost = await newPost.save();
+        return new NextResponse(" The post has been created");
+    }catch(err){
+        return new NextResponse("Database Error", {status:500});
+    }
 }
 
--- portfolio_next.js/src/app/page.jsx
+++ portfolio_next.js/src/app/page.jsx
@@ -1,8 +1,7 @@
 import Image from 'next/image'
 import styles from './page.module.css';
-import Hero from 'public/hero.png';
 import Button from '@/components/Button/Button';
-
+import Hero from '/public/hero.png'
 export default function Home() {
   return (
     <div className={ styles.container}>
